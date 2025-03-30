3be97b5e8b66dc448abfe1c614e14e6c5ed46217
josiahcoad <josiah@coad.net>
2025-03-29 21:45:21 -0500

    pretty print

--- agents/helloworld/helloworld_agent.py
+++ agents/helloworld/helloworld_agent.py
@@ -1,32 +1,35 @@
+import os
+
 from dotenv import load_dotenv
 from langchain.chat_models import init_chat_model
+from langchain_community.tools.tavily_search import TavilySearchResults
 from langgraph.prebuilt import create_react_agent
 
 from agents.helloworld.tools import add, subtract
 
 load_dotenv()
 
-# for searching the web
-# tavily_search = TavilySearchResults(max_results=5)
+if os.getenv("TAVILY_API_KEY"):
+    tavily_search = TavilySearchResults(max_results=5)
+else:
+    tavily_search = None
 
 llm = init_chat_model(model="gpt-4o", temperature=0)
 
 # Create the agent with custom system prompt
-system_prompt = """
-You talk like a pirate.
-"""
+system_prompt = "Talk like a pirate."
 
 
 graph = create_react_agent(
     llm,
     tools=[
-        # tavily_search,
         add,
         subtract,
+        tavily_search,
     ],
     prompt=system_prompt,
 )
 
 if __name__ == "__main__":
-    response = graph.invoke({"message": "What is 10 + 10?"})
-    response["messages"][-1].pretty_print()
+    response = graph.invoke({"messages": ["What is 2+2?"]})
+    print(response)
